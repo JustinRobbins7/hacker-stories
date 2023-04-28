eacd36b7e44850cc9bbb5086dc14c29e1f1e253a
JustinRobbins7 <robbinsju@gmail.com>
2023-04-28 16:44:22 -0500

    Section 10 - Lists in React

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const title = 'React'
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 
 function App() {
-  // You can do something in between the function declaration and the return statement
-  // Variables declared within the App function will be reassigned upon rendering
-  // If variable is not dependent on things internal to the function, declare it outside of it.
 
   return (
     <div>
-      <h1>title</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text"/>
+
+      <hr/>
+
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} </a>
+              </span>
+              <span>{item.author} </span>
+              <span>{item.num_comments} </span>
+              <span>{item.points} </span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
