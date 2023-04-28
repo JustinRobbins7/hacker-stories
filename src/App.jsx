3cd354831eae7567aba0b9a16e8108e0f30eb346
JustinRobbins7 <robbinsju@gmail.com>
2023-04-28 17:32:22 -0500

    Section 13 - React Component Declaration

--- src/App.jsx
+++ src/App.jsx
@@ -22,35 +22,30 @@ const list = [
   }
 ]
 
-function App() {
-
-  return (
+const App = () => (
     <div>
       <h1>My Hacker Stories</h1>
 
-      
+      <Search/>
 
       <hr/>
 
       <List/>
     </div>
   );
-}
 
-function Search() {
-  return (
+
+const Search = () => (
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text"/>
     </div>
   );
-}
 
-function List() {
-  return (
+
+const List = () => (
     <ul>
-        {list.map(function (item) {
-          return (
+        {list.map((item) => (
             <li key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title} </a>
@@ -59,10 +54,9 @@ function List() {
               <span>{item.num_comments} </span>
               <span>{item.points} </span>
             </li>
-          );
-        })}
+          )
+        )}
       </ul>
   );
-}
 
 export default App
