f023790c48c7b798bb102dbf423fe0c8876f01cd
JustinRobbins7 <robbinsju@gmail.com>
2023-05-02 17:09:42 -0500

    Section 27 - Inline Handler JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,25 @@
 import './App.css'
 import React from 'react'
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://react.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 // Custom Hook
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(localStorage.getItem(key) || initialState)
@@ -14,32 +33,20 @@ const useStorageState = (key, initialState) => {
 }
 
 const App = () => {
-
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://react.org',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ]
-
+  const [stories, setStories] = React.useState(initialStories) 
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React')
 
   const handleSearch = (event) => {
     console.log(event.target.value)
     // Set value
     setSearchTerm(event.target.value)
+  }
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    )
+    setStories(newStories)
   }
 
   const searchedStories = stories.filter(
@@ -67,6 +74,7 @@ const App = () => {
       <List 
         searchTerm={searchTerm} 
         list={searchedStories} 
+        onRemoveItem={handleRemoveStory}
       />
 
     </div>
@@ -88,23 +96,28 @@ const InputWithLabel = ({id, type = 'text', value, isFocused, onInputChange, chi
   );
 
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveItem }) => (
     <ul>
         {list.map((item) => (
-            <Item key={item.objectID} item={item} />
+            <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
         ))}
       </ul>
   );
 
-const Item = ({ item }) => (
-  <li>
-    <span>
-      <a href={item.url}>{item.title} </a>
-    </span>
-    <span>{item.author} </span>
-    <span>{item.num_comments} </span>
-    <span>{item.points} </span>
-  </li>
-)
+const Item = ({ item, onRemoveItem }) => (
+    <li>
+      <span>
+        <a href={item.url}>{item.title} </a>
+      </span>
+      <span>{item.author} </span>
+      <span>{item.num_comments} </span>
+      <span>{item.points} </span>
+      {/* Bind arguments directly into handler callback to avoid declaration of a handler in Item with java .bind (onRemoveItem.bind(null, item)) or arrow function*/}
+      <button type="button" onClick={() => onRemoveItem(item)}>Dismiss</button>
+    </li>
+  )
+
+
+  
 
 export default App
